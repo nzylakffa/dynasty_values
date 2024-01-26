33b63530d053b1c736ef27397400700c6c360835
nzylakffa <131708217+nzylakffa@users.noreply.github.com>
2024-01-26 11:08:21 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,6 +6,19 @@ import pandas as pd
 import time
 import streamlit as st
 
+st.sidebar.markdown("# Read This!")
+st.sidebar.markdown("### You will see errors until you fill out the 4 items on this page and then select your team and a trade partner on the Trade Calculator tab!")
+st.sidebar.markdown("1) Click Fullscreen at the bottom for a better user experience")
+st.sidebar.markdown("2) Input Sleeper Username")
+st.sidebar.markdown("3) Input Season")
+st.sidebar.markdown("(This is the season you're looking at. Use 2023 for last season and 2024 once we draft our teams for 2024.)")
+st.sidebar.markdown("4) Select the league you want to use")
+st.sidebar.markdown("(This is a dropdown of all the league's you're in! If you don't know which is which then just pick one and check out the trade calculator tab to see which team of yours that is.)")
+st.sidebar.markdown("5) Input your league's scoring format")
+st.sidebar.markdown("6) Turn on the toggle if it's a dynasty league")
+st.sidebar.markdown("7) Go to the Trade Calculator tab and select your display name along with your trade partner's display name")
+st.sidebar.markdown("8) You'll need to wait a few seconds for the tool to pull all the information")
+
 # Set up Chrome options for headless mode
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run Chrome in headless mode
