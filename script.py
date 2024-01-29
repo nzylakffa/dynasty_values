4acdc7d521a828cd6c57a9aaa2b729bf7484d259
nzylakffa <131708217+nzylakffa@users.noreply.github.com>
2024-01-29 17:11:39 -0500

    Update script.py

--- script.py
+++ script.py
@@ -12,13 +12,18 @@ from matplotlib.colors import ListedColormap
 import matplotlib
 import streamlit as st
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 ###################
 ##### Sidebar #####
